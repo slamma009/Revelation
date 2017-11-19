57931c6b396019af09287499d6c2feb5f8d2f952
slamma009 <alsnurb@gmail.com>
2017-11-18 19:01:22 -0600

    Bittrex Added

--- Global.js
+++ Global.js
@@ -1,3 +1,37 @@
+const CryptoJS = require("crypto-js");
+
 function hasKey(arr, key){
     return arr[key] !== undefined;
+}
+
+function webCall(url, account){
+    var startChar = '?';
+    if(url.includes('?'))
+        startChar = '&';
+    var secret = ''
+    if(account !== undefined){
+        var nonce = Math.round((new Date()).getTime() / 1000);
+        url += startChar + 'apikey=' + account.API + '&nonce=' + nonce;
+        secret = CryptoJS.HmacSHA512(url, account.Secret).toString(CryptoJS.enc.Hex);
+    }
+    return $.ajax({
+        type: 'GET',
+        headers: { 
+            'apisign': secret
+        },
+        url: url,
+        dataType: 'json',
+        success: function(response) {
+            return response;
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
+
+function checkLogin(successFunction, account){
+    webCall('https://bittrex.com/api/v1.1/account/getbalances', account).then(function(d){
+        successFunction(d.success);
+    });
 }

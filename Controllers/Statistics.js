fa1226cfb82b46e3cf467bc4cc373e870d62454e
slamma009 <alsnurb@gmail.com>
2017-11-20 01:08:25 -0600

    Statistics page imiprovements
    Amount of API calls greatly reduced, need to clean code when it's not 1
    A.M.

--- Controllers/Statistics.js
+++ Controllers/Statistics.js
@@ -16,6 +16,9 @@ function statisticsController($scope){
     $scope.WeeklyGains = [];
     $scope.DailyGains = [];
     $scope.MonthlyGains = [];
+    $scope.day = {};
+    $scope.week = {};
+    $scope.month = {};
     $scope.$watch('walletHolder', function(walletHolder){
 
         if(walletHolder !== undefined && walletHolder.allTickers !== undefined){
@@ -46,36 +49,81 @@ function statisticsController($scope){
     }
 
     function getGains(timestamp, ticker, days) {
-        cryptoCompareCall('pricehistorical?fsym=' + ticker + '&tsyms=BTC,USD&ts=' + timestamp + '&extraParams=Revelation').then(function(response){
-            var properties = [];
-            for(var key in response) {
-                if(response.hasOwnProperty(key) && typeof response[key] !== 'function') {
-                    properties.push(key);
-                }
-            } 
-            for(var i=0; i<$scope.allCoins.length; ++i){
-                if($scope.allCoins[i].ticker === properties[0])
-                {
-                    var gain = $scope.walletHolder[properties[0]].usdMarketValue - response[properties[0]].USD;
-                    gain = (gain / response[properties[0]].USD) * 100;
-                    if(days === 30){
-                        $scope.allCoins[i].monthlyGain = Math.round(gain * 100 ) / 100;
-                    } else if(days === 7) {
-                        $scope.allCoins[i].weeklyGain = Math.round(gain * 100 ) / 100;
-                    } else if(days === 1){
-                        $scope.allCoins[i].dailyGain = Math.round(gain * 100 ) / 100;
+        if((days === 30 && ($scope.month.timestamp !== timestamp || $scope.month[ticker] === undefined))  ||
+        (days === 7 && ($scope.week.timestamp !== timestamp|| $scope.week[ticker] === undefined)) || 
+        (days === 1 && ($scope.day.timestamp !== timestamp|| $scope.day[ticker] === undefined))) {
+            cryptoCompareCall('pricehistorical?fsym=' + ticker + '&tsyms=BTC,USD&ts=' + timestamp + '&extraParams=Revelation').then(function(response){
+                var properties = [];
+                for(var key in response) {
+                    if(response.hasOwnProperty(key) && typeof response[key] !== 'function') {
+                        properties.push(key);
                     }
-                    break;
+                } 
+                for(var i=0; i<$scope.allCoins.length; ++i){
+                    if($scope.allCoins[i].ticker === properties[0])
+                    {
+                        var gain = $scope.walletHolder[properties[0]].usdMarketValue - response[properties[0]].USD;
+                        gain = (gain / response[properties[0]].USD) * 100;
+                        if(days === 30){
+                            $scope.month.timestamp = timestamp;
+                            $scope.month[ticker] = response[properties[0]].USD;
+                            $scope.allCoins[i].monthlyGain = Math.round(gain * 100 ) / 100;
+                        } else if(days === 7) {
+                            $scope.week.timestamp = timestamp;
+                            $scope.week[ticker] = response[properties[0]].USD;
+                            $scope.allCoins[i].weeklyGain = Math.round(gain * 100 ) / 100;
+                        } else if(days === 1){
+                            $scope.day.timestamp = timestamp;
+                            $scope.day[ticker] = response[properties[0]].USD;
+                            $scope.allCoins[i].dailyGain = Math.round(gain * 100 ) / 100;
+                        }
+                        break;
+                    }
                 }
-            }
-            if(days === 30) {
+                if(days === 30) {
+                    calculateMonthlyGains();
+                } else if(days === 7) {
+                    calculateWeeklyGains();
+                } else if(days === 1){
+                    calculateDailyGains();
+                }
+            });
+        } else {
+            if(days === 30) { 
+                var gain = $scope.walletHolder[ticker].usdMarketValue - $scope.month[ticker];
+                gain = (gain / $scope.month[ticker]) * 100;
+                for(var i=0; i<$scope.allCoins.length; ++i){
+                    if($scope.allCoins[i].ticker === ticker)
+                    {
+                        $scope.allCoins[i].monthlyGain = Math.round(gain * 100)/100;
+                        break;
+                    }
+                }
                 calculateMonthlyGains();
             } else if(days === 7) {
+                var gain = $scope.walletHolder[ticker].usdMarketValue - $scope.week[ticker];
+                gain = (gain / $scope.week[ticker]) * 100;
+                for(var i=0; i<$scope.allCoins.length; ++i){
+                    if($scope.allCoins[i].ticker === ticker)
+                    {
+                        $scope.allCoins[i].weeklyGain = Math.round(gain * 100)/100;
+                        break;
+                    }
+                }
                 calculateWeeklyGains();
             } else if(days === 1){
+                var gain = $scope.walletHolder[ticker].usdMarketValue - $scope.day[ticker];
+                gain = (gain / $scope.day[ticker]) * 100;
+                for(var i=0; i<$scope.allCoins.length; ++i){
+                    if($scope.allCoins[i].ticker === ticker)
+                    {
+                        $scope.allCoins[i].dailyGain = Math.round(gain * 100)/100;
+                        break;
+                    }
+                }
                 calculateDailyGains();
             }
-        });
+        }
     }
     function calculateMonthlyGains() {
         var MonthlyGains = angular.copy($scope.allCoins).sort(function(a, b){
@@ -88,7 +136,6 @@ function statisticsController($scope){
 
         $scope.MonthlyGains = MonthlyGains;
 
-        $scope.$apply();
     }
     function calculateWeeklyGains() {
         var WeeklyGains = angular.copy($scope.allCoins).sort(function(a, b){
@@ -101,7 +148,6 @@ function statisticsController($scope){
 
         $scope.WeeklyGains = WeeklyGains;
 
-        $scope.$apply();
     }
     function calculateDailyGains() {
         var DailyGains = angular.copy($scope.allCoins).sort(function(a, b){
@@ -115,7 +161,6 @@ function statisticsController($scope){
         $scope.DailyGains = DailyGains;
 
 
-        $scope.$apply();
     }
 
     function getTimestamp(days){

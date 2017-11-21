2a10b0128007297141569409ed2df02a6f6187b8
slamma009 <alsnurb@gmail.com>
2017-11-21 14:52:42 -0600

    Fixed check for market prices
    Originally depended that the USDT-BTC market would be hit after
    all
    coins where valued. Changed to make sure this happens.

--- AngularMain.js
+++ AngularMain.js
@@ -68,6 +68,7 @@ app.controller('mainController', function ($scope){
     // Get's the market value of each coin
     function getMarketValues(){
         var usdValue = 0;
+        var btcValue = 0;
         var tickersToRemove = []; // Keep a list of tickers that are practically worthless, so we can remove them
         for(var i=0; i<$scope.marketSummaries.length; ++i){
             // Get the base and market ticker
@@ -81,33 +82,37 @@ app.controller('mainController', function ($scope){
                     $scope.walletHolder[marketTicker].btcMarketValue = $scope.marketSummaries[i].Last;
                 }
             } else if(baseTicker === 'USDT' && marketTicker === 'BTC'){
-                // Now that we have the BTC USDT price, lets calculat ethe USD value for all coins
                 btcValue = $scope.marketSummaries[i].Last;
-                for(var j=0; j<$scope.walletHolder.allTickers.length; ++j){
-                    // If the coin is not BTC we have to convert to BTC, then to USD
-                    if($scope.walletHolder.allTickers[j] !== 'BTC'){
-                        $scope.walletHolder[$scope.walletHolder.allTickers[j]].usdValue = Math.round($scope.walletHolder[$scope.walletHolder.allTickers[j]].btcValue * btcValue * 100) / 100;
-                        $scope.walletHolder[$scope.walletHolder.allTickers[j]].usdMarketValue = Math.round($scope.walletHolder[$scope.walletHolder.allTickers[j]].btcMarketValue * btcValue * 100) / 100;
-                        if($scope.walletHolder[$scope.walletHolder.allTickers[j]].usdValue < 0.5)
-                        {
-                            $scope.walletHolder[$scope.walletHolder.allTickers[j]] = undefined;
-                            tickersToRemove.push(j);
-                        } else {
-                            usdValue += $scope.walletHolder[$scope.walletHolder.allTickers[j]].usdValue;
-                        }
-                    } else {
-                        $scope.walletHolder['BTC'].btcValue =  $scope.walletHolder['BTC'].quantity;
-                        $scope.walletHolder['BTC'].usdValue =  Math.round($scope.walletHolder['BTC'].btcValue * btcValue * 100) / 100;
-                        $scope.walletHolder['BTC'].usdMarketValue = Math.round(btcValue * 100) / 100;
-                        usdValue += $scope.walletHolder['BTC'].usdValue;
-                    }
-                }
             }
         }
+
+        // Now that we have the market prices for all coins, lets calculat ethe USD value for all coins
+        for(var j=0; j<$scope.walletHolder.allTickers.length; ++j){
+            // If the coin is not BTC we have to convert to BTC, then to USD
+            if($scope.walletHolder.allTickers[j] !== 'BTC'){
+                $scope.walletHolder[$scope.walletHolder.allTickers[j]].usdValue = Math.round($scope.walletHolder[$scope.walletHolder.allTickers[j]].btcValue * btcValue * 100) / 100;
+                $scope.walletHolder[$scope.walletHolder.allTickers[j]].usdMarketValue = Math.round($scope.walletHolder[$scope.walletHolder.allTickers[j]].btcMarketValue * btcValue * 100) / 100;
+                if($scope.walletHolder[$scope.walletHolder.allTickers[j]].usdValue < 0.5)
+                {
+                    $scope.walletHolder[$scope.walletHolder.allTickers[j]] = undefined;
+                    tickersToRemove.push(j);
+                } else {
+                    usdValue += $scope.walletHolder[$scope.walletHolder.allTickers[j]].usdValue;
+                }
+            } else {
+                $scope.walletHolder['BTC'].btcValue =  $scope.walletHolder['BTC'].quantity;
+                $scope.walletHolder['BTC'].usdValue =  Math.round($scope.walletHolder['BTC'].btcValue * btcValue * 100) / 100;
+                $scope.walletHolder['BTC'].usdMarketValue = Math.round(btcValue * 100) / 100;
+                usdValue += $scope.walletHolder['BTC'].usdValue;
+            }
+        }
+
+        //remove any coins that are worth little to nothing.
         for(var i=tickersToRemove.length - 1; i >= 0; --i){
             $scope.walletHolder.allTickers.splice(tickersToRemove[i], 1);
         }
         usdValue = Math.round(usdValue * 100) / 100;
+        
         var $target = $("#usdDisplay");
         
         if(usdValue > $scope.usdValue){

69152c5579230ac6e2f225410d3cfff43b904424
slamma009 <alsnurb@gmail.com>
2017-11-22 01:57:26 -0600

    Removing hacky changes, too hacky
    It looks like that using 3 datasets to get the proper colorscheme is not
    considered a good solution, and may cause issues with the display.

--- Controllers/Portfolio.js
+++ Controllers/Portfolio.js
@@ -21,10 +21,10 @@ function portfolioController($scope, Settings){
             }]
         },
         options: {
-            cutoutPercentage: 70,
+            cutoutPercentage: 80,
             elements: {
                 arc: {
-                    borderWidth: 1
+                    borderWidth: 0
                 }
             },
             animation : false,  
@@ -52,42 +52,32 @@ function portfolioController($scope, Settings){
             $scope.chart.data.labels = holder.allTickers;
 
             // Create the data set object and put in the usdValues
-            var dataset = {label:'', backgroundColor: [], data: []}
+            var dataset = {label:'', backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"], data: []}
             for(var i=0; i<holder.allTickers.length; ++i){
                 dataset.data.push(holder[holder.allTickers[i]].usdValue);
             }
 
             // Generate a color rainbow for the pie chart
             $scope.rainbow = new Array(holder.allTickers.length);
-            $scope.rainbowInner = new Array(holder.allTickers.length);
             for (var i=0; i<holder.allTickers.length; i++) {
             var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
             var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
             var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
             
-            $scope.rainbow[i] = "#"+ red[0] + green[0] + blue[0];
-            $scope.rainbowInner[i] = "#"+ red[1] + green[1] + blue[1];
+            $scope.rainbow[i] = "#"+ red + green + blue;
             }
 
             function sin_to_hex(i, phase) {
             var sin = Math.sin(Math.PI / holder.allTickers.length * 2 * i + phase);
-            var int = Math.floor((Math.floor(sin * 127) + 128) * 1);
-            var darkerInt = Math.floor((Math.floor(sin * 127) + 128) * 0.75); // Multiply by 0.7 to make the colors darker.
+            var int = Math.floor((Math.floor(sin * 127) + 128) * 1); // Multiply by 0.7 to make the colors darker.
             var hex = int.toString(16);
-            var darkerHex = darkerInt.toString(16);
-            var returnItem = [];
-            returnItem.push(hex.length === 1 ? "0"+hex : hex);
-            returnItem.push(darkerHex.length === 1 ? "0"+darkerHex : darkerHex);
-            return returnItem;
+            
+            return hex.length === 1 ? "0"+hex : hex;
             }
 
             // Set the color rainbow to the background colors, and add the dataset to the chart
-            var datasetDarker = angular.copy(dataset);
-            datasetDarker.backgroundColor = $scope.rainbowInner;
-            datasetDarker.borderColor = $scope.rainbowInner; 
             dataset.backgroundColor = $scope.rainbow; 
-            dataset.borderColor = $scope.rainbow;
-            $scope.chart.data.datasets = [angular.copy(dataset), dataset, datasetDarker];
+            $scope.chart.data.datasets = [dataset];
 
             //update the chart
             $scope.chart.update();

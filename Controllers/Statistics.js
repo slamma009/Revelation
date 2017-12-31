c0d2e8918c4c571320ac1dfbb23e047dfdea2640
slamma009 <alsnurb@gmail.com>
2017-12-31 15:55:35 -0600

    Added Top Gains
    Changed current statistics to be coins that you are currently holding,
    and added top ones from the entire market.

--- Controllers/Statistics.js
+++ Controllers/Statistics.js
@@ -4,43 +4,59 @@ angular.module('revelation')
         templateUrl: 'Controllers/Statistics.html',
         controller: statisticsController,
         scope: {
-            walletHolder: '='
+            walletHolder: '=',
+            marketSummaries: '='
         }
     };
 });
 
 function statisticsController($scope, Settings){
   
-    $scope.allCoins = [];
-    $scope.MostValuableCoins = [];
-    $scope.gainsHolder = {};
+    $scope.coinsHolder = {};
+    $scope.coinsHolder.allCoins = [];
+    $scope.coinsHolder.heldCoins = [];
+    $scope.allCoins = {gainsHolder: {}, MostValuableCoins: []};
+    $scope.heldCoins = {gainsHolder: {}, MostValuableCoins: []};
     $scope.$watch('walletHolder', function(walletHolder){
 
         if(walletHolder !== undefined && walletHolder.allTickers !== undefined){
-            $scope.allCoins = [];
+            $scope.coinsHolder.heldCoins = [];
             for(var i=0; i<walletHolder.allTickers.length; ++i){
-                $scope.allCoins.push({ticker: walletHolder.allTickers[i], wallet: walletHolder[walletHolder.allTickers[i]]});
+                $scope.coinsHolder.heldCoins.push({ticker: walletHolder.allTickers[i], wallet: walletHolder[walletHolder.allTickers[i]]});
             }
-            calculateGains('percent_change_1h');
-            calculateGains('percent_change_24h');
-            calculateGains('percent_change_7d');
+            calculateGains('heldCoins', 'percent_change_1h');
+            calculateGains('heldCoins', 'percent_change_24h');
+            calculateGains('heldCoins', 'percent_change_7d');
 
-            calculateValuable();
+            calculateValuable('heldCoins');
             
         }
     });
+    $scope.$watch('marketSummaries', function(marketSummaries){
+        if(marketSummaries !== undefined && marketSummaries.length > 0){
+            $scope.coinsHolder.allCoins = [];
+            for(var i=0; i<marketSummaries.length; ++i){
+                $scope.coinsHolder.allCoins.push({ticker: marketSummaries[i].symbol, wallet: marketSummaries[i]});
+            }
+            calculateGains('allCoins', 'percent_change_1h');
+            calculateGains('allCoins', 'percent_change_24h');
+            calculateGains('allCoins', 'percent_change_7d');
 
-    function calculateValuable(){
-        $scope.MostValuableCoins = angular.copy($scope.allCoins).sort(function(a, b){
+            calculateValuable('allCoins');
+        }
+    });
+
+    function calculateValuable(coins){
+        $scope[coins].MostValuableCoins = angular.copy($scope.coinsHolder[coins]).sort(function(a, b){
             return b.wallet['price_' + $scope.tickerToShow] - a.wallet['price_' + $scope.tickerToShow];
         });
     }
 
     
-    function calculateGains(propertyName) {
-        if($scope.allCoins.length > 0 && $scope.allCoins[0].wallet[propertyName] !== undefined)
+    function calculateGains(coins, propertyName) {
+        if($scope.coinsHolder[coins].length > 0 && $scope.coinsHolder[coins][0].wallet[propertyName] !== undefined)
         {
-            var GainsTemp = angular.copy($scope.allCoins).sort(function(a, b){
+            var GainsTemp = angular.copy($scope.coinsHolder[coins]).sort(function(a, b){
                 return b.wallet[propertyName] - a.wallet[propertyName];
             });
             
@@ -50,7 +66,7 @@ function statisticsController($scope, Settings){
                 Gains.push(coin);
             }
 
-            $scope.gainsHolder[propertyName] = Gains;
+            $scope[coins].gainsHolder[propertyName] = Gains.slice(0,20);
         }
 
     }
